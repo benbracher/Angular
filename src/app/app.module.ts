e822b0b50865e6ac28a9ae69d8aafc5c26955def
gngo2018 <gngo2018@gmail.com>
2018-12-20 13:30:33 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { DisplayBooksComponent } from './display-books/display-books.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+
+// ng g c "" in command line makes new terminal
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayBooksComponent,
+    CreateBookComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/display-books/display-books.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component ({
+    selector: 'app-display-books',
+//     template: `<h1>This Display Books Component is Working!</h1>`,
+    templateUrl: './display-books.component.html',
+    styleUrls: ['./display-books.component.css'], 
+
+})
+
+export class DisplayBooksComponent {}
